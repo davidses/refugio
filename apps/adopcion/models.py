8485165408bcdee6d45a0d4c9caa66262deeaf22
Adrian Rosa <cabeza_mtz@hotmail.com>
2018-12-17 18:20:59 -0300

    primer commit

---
+++ apps/adopcion/models.py
@@ -0,0 +1,14 @@
+# django asigna un campo ID autoincrementable automaticamente 
+# a menos que nosotros creemos un campo con clave primaria.
+
+from django.db import models
+
+class Persona(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=70)
+    edad = models.IntegerField()
+    telefono = models.CharField(max_length=12)
+    email = models.EmailField()
+    domicilio = models.TextField()
+
+
