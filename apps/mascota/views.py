52bae6977feb2a5067658f0a355f222c21594d68
Adrian Rosa <cabeza_mtz@hotmail.com>
2018-12-20 18:38:11 -0300

    Clase 13

--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from apps.mascota.forms import MascotaForm
+from apps.mascota.models import Mascota
 
 # Create your views here.
 
@@ -10,7 +11,7 @@ def index(request):
 
 def mascota_view(request):
     if request.method == "POST":
-        form = MascotaForm(request.POST)
+        form = MascotaForm(request.POST) 
         if form.is_valid():
             form.save()
         return redirect("mascota:index")
@@ -18,4 +19,8 @@ def mascota_view(request):
         form = MascotaForm()
 
     return render(request, "mascota/mascota_form.html", {"form":form})
-            
+
+def mascota_list(request):
+    mascota = Mascota.objects.all()
+    contexto = {"mascotas":mascota}
+    return render(request, "mascota/mascota_list.html", contexto)
