99c77ca6e2df8ef1215a933f37c9eda705688769
Adrian Rosa <cabeza_mtz@hotmail.com>
2018-12-27 10:30:09 -0300

    clase 15

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url, include
 
-from apps.mascota.views import index, mascota_view, mascota_list, mascota_edit, mascota_delete
+from apps.mascota.views import index, mascota_view, mascota_list, mascota_edit, mascota_delete, \
+    MascotaList, MascotaCreate
 
 urlpatterns = [
     url(r'^$', index, name="index"),
-    url(r'^nuevo$', mascota_view, name="mascota_crear"),
-    url(r'^listar$', mascota_list, name="mascota_listar"),
+    url(r'^nuevo$', MascotaCreate.as_view(), name="mascota_crear"),
+    url(r'^listar$', MascotaList.as_view(), name="mascota_listar"),
     url(r'^editar/(?P<id_mascota>\d+)/$', mascota_edit, name="mascota_editar"),
     url(r'^eliminar/(?P<id_mascota>\d+)/$', mascota_delete, name="mascota_eliminar"),
 ]
--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import ListView, CreateView
 
 from apps.mascota.forms import MascotaForm
 from apps.mascota.models import Mascota
@@ -9,21 +11,24 @@ from apps.mascota.models import Mascota
 def index(request):
     return render(request, "mascota/index.html")
 
+
 def mascota_view(request):
     if request.method == "POST":
         form = MascotaForm(request.POST) 
         if form.is_valid():
             form.save()
-        return redirect("mascota:index")
+        return redirect("mascota:mascota_listar")
     else:
         form = MascotaForm()
 
     return render(request, "mascota/mascota_form.html", {"form":form})
 
+
 def mascota_list(request):
     mascota = Mascota.objects.all().order_by("id")
     contexto = {"mascotas":mascota}
     return render(request, "mascota/mascota_list.html", contexto)
+
 
 def mascota_edit(request, id_mascota):
     mascota = Mascota.objects.get(id=id_mascota)
@@ -36,9 +41,23 @@ def mascota_edit(request, id_mascota):
         return redirect("mascota:mascota_listar")
     return render(request, "mascota/mascota_form.html", {"form":form})
 
+
 def mascota_delete(request, id_mascota):
     mascota = Mascota.objects.get(id=id_mascota)
     if request.method == "POST":
         mascota.delete()
         return redirect("mascota:mascota_listar")
     return render(request, "mascota/mascota_delete.html", {"mascota":mascota})
+
+
+# VISTAS BASADAS EN CLASES
+
+class MascotaList(ListView):
+    model = Mascota
+    template_name = "mascota/mascota_list.html"
+
+class MascotaCreate(CreateView):
+    model = Mascota
+    form_class = MascotaForm
+    template_name = "mascota/mascota_form.html"
+    success_url = reverse_lazy("mascota:mascota_listar")
